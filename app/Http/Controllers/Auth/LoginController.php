09040e9f00f68a4de23a7aad407c17e3fbd1e43d
Htet Phone <senseihtetphone@gmail.com>
2024-06-12 23:22:57 +0630

    phone_no | login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    //chaing login option from email to phone_no
+    public function username()
+    {
+        return 'phone_no';
+    }
 }
